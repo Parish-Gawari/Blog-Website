26be00e8a67775f433eb4c41fa82227f66531cb8
Parish Gawari <parishk1024@gmail.com>
2024-08-14 20:51:25 +0530

    connected db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log("Server is running on Port 3000");
 });
