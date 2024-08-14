bb3601f66fbd7d6e21297f6d7c23b2e8d8656d67
Parish Gawari <parishk1024@gmail.com>
2024-08-14 17:32:12 +0530

    added hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,9 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 const Header = () => {
+  const path = useLocation().pathname;
   return (
     <Navbar>
       <Link
@@ -25,11 +26,26 @@ const Header = () => {
       <Button className="w-12 h-10 lg:hidden" color={"grey"} pill>
         <AiOutlineSearch />
       </Button>
-      <div>
+      <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="grey" pill>
           <FaMoon />
         </Button>
+        <Link to="/sign-in">
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
       </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
