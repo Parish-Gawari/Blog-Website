b5dc038164f8dcb851df9c7ceae695a21b057fa4
Parish Gawari <parishk1024@gmail.com>
2024-09-06 12:36:38 +0530

    frontend done for posts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import flowbite from "flowbite-react/tailwind";
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.content()],
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
 };
